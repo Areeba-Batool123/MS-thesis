8ba2180bfba7c4b406ad1965ff0eedc6ccd77f25
Dark world <131669303+AsifaSiraj@users.noreply.github.com>
2025-01-20 23:33:19 +0500

    home complete
    sirf footer me new page reference reh gaya hai wo dekh lena mujhe nahi araha me agay login page dekh rahi hon

---
+++ src/Home/Bodyb.jsx
@@ -0,0 +1,152 @@
+function Body3sec() {
+    return (
+        <>
+            <div className="flex justify-center items-center px-8 "> {/* Main container with background color and padding */}
+                <div className="flex flex-col items-center w-full max-w-7xl  rounded-3xl shadow-lg "> {/* Card container */}
+                    {/* <h2 className="text-3xl font-bold text-white mb-4 text-center">Online Thesis Management System that caters to everyone's needs</h2> Main title */}
+                    
+                    <h1
+            className="relative max-w-full font-extrabold text-center pt-0 mx-auto text-transparent bg-clip-text ml-10 bg-gradient-to-r from-[#4A90E2] to-[#002B5C] my-1 mb-5 leading-snug"
+            // Ensures the heading doesn't wrap
+          >
+            <div  style={{ fontSize: '3rem', whiteSpace: 'nowrap' }}>Online Thesis Management System that caters</div>
+            <div  style={{ fontSize: '3rem', whiteSpace: 'nowrap' }}>to everyone's needs</div>
+          </h1>
+                    
+                    <p className="text-lg text-gray-300 mb-6 text-center"></p> {/* Subtitle */}
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full"> {/* Grid for features */}
+                        <div className="p-6 bg-black rounded-2xl h-80 hover:scale-105 hover:bg-gradient-to-r hover:from-blue-600 hover:to-blue-800 transition-transform duration-300 ease-in-out"> {/* Feature 1 */}
+                            <h3 className="text-xl font-semibold text-white mb-4 ">Research Scholar</h3> {/* Feature title */}
+                            <p className="text-gray-400">No more frustration - right from initial application, guide selection to DC invite, courses, publications, synopsis, and final thesis submission and viva, everything is automated.</p> {/* Feature description */}
+                        </div>
+                        <div className="p-6 bg-black rounded-2xl h-80 hover:scale-105 hover:bg-gradient-to-r hover:from-blue-600 hover:to-blue-800 transition-transform duration-300 ease-in-out"> {/* Feature 2 */}
+                            <h3 className="text-xl font-semibold text-white mb-4 ">Research Guide</h3> {/* Feature title */}
+                            <p className="text-gray-400">Constructs a bridge between scholars and guides with easy communication channels. Easy acceptance and approval of the coursework, with immediate review processes.</p> {/* Feature description */}
+                        </div>
+                        <div className="p-4 bg-black rounded-2xl h-80 hover:scale-105 hover:bg-gradient-to-r hover:from-blue-600 hover:to-blue-800 transition-transform duration-300 ease-in-out"> {/* Feature 3 */}
+                            <h3 className="text-xl font-semibold text-white mb-4 ">Research Committee Coordinator</h3> {/* Feature title */}
+                            <p className="text-gray-400">Keep track of the entire thesis process in a centralized place. Facilitates research interviews, coordination with external reviewers, and final approvals/rejection in just a click.</p> {/* Feature description */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+{/* image part */}
+<h1
+  className="pt-10 pb-10 relative max-w-full font-extrabold text-center mx-auto text-transparent bg-clip-text bg-gradient-to-r from-[#4A90E2] to-[#002B5C] my-1 mb-5 leading-snug"
+   // Ensures the heading doesn't wrap
+>
+  <div style={{ fontSize: '3rem', whiteSpace: 'nowrap' }}>
+    Thesis Management System, for on-the-go thesis, </div>
+    <div style={{ fontSize: '3rem', whiteSpace: 'nowrap' }}>approvals, and submissions</div>
+</h1>
+
+<div className="p-9 rounded-2xl h-auto  transition-transform duration-300 ease-in-out hover:scale-95 hover:text-white flex items-center space-x-6"
+style={{ fontSize: '23px', whiteSpace: 'nowrap' }}
+>
+  {/* Text Section */}
+  <div className="text-section flex-1">
+    <h3 className="text-xl font-semibold mb-3 transition-colors duration-300 text-bluish"
+    style={{ fontSize: '30px'}}
+    >
+      Make thesis registration effortless
+    </h3>
+    <div className="text-gray-300 transition-colors duration-300">
+      Reduce long wait lines of your thesis registration process from the applications, 
+    </div>
+    <div className="text-gray-300 transition-colors duration-300">
+      
+    </div>
+    <div className="text-gray-300 transition-colors duration-300">
+      verification, PET score updation, and interviews to shortlisting and guide allocation,
+    </div>
+    <div className="text-gray-300 transition-colors duration-300">
+    our thesis management system puts your admission committee in charge.
+    </div>
+    <div className="text-gray-300 transition-colors duration-300">
+      The system is intelligent enough to handle multiple statuses just in a few clicks.
+    </div>
+    <div className="text-gray-300 transition-colors duration-300">
+      With automated processes and reporting tools, your admission team can stay empowered 
+    </div>
+    <div className="text-gray-300 transition-colors duration-300"> 
+      throughout their thesis registration cycle.
+    </div>
+    <div className="text-gray-300 transition-colors duration-300">
+      <strong>Do more with Creatrix:</strong>
+    </div>
+    <ul className="list-disc list-inside text-gray-300 transition-colors duration-300">
+      <li>Reminders to speed up registrations</li>
+      <li>Online thesis registration forms</li>
+      <li>Status management for submissions</li>
+    </ul>
+  </div>
+
+  {/* Image Section */}
+  <div className="image-section">
+    <img 
+      src="Make-registration-simpler.webp" 
+      alt="Description" 
+      style={{ width: '150%', height: '150%' }}
+    />
+  </div>
+</div>
+
+
+<div className="p-20 rounded-2xl h-auto  transition-transform duration-300 ease-in-out hover:scale-95 hover:text-white flex items-center space-x-6"
+style={{ fontSize: '23px', whiteSpace: 'nowrap' }}
+>
+  {/* Text Section 2 */}
+  <div className="text-section flex-1">
+    <h3 className="text-xl font-semibold mb-3 transition-colors duration-300 text-bluish"
+    style={{ fontSize: '30px'}}
+    >
+      Keep publications on track
+    </h3>
+    <div className="text-gray-300 transition-colors duration-300">
+    Compile your thesis publication process with a high-quality  
+    </div>
+    <div className="text-gray-300 transition-colors duration-300">
+    repository that tracks all research publications.
+    </div>
+    <div className="text-gray-300 transition-colors duration-300">
+    Input all details associated with the publication along with remarks,
+    </div>
+    <div className="text-gray-300 transition-colors duration-300">
+    so your scholarly efforts are in one place.
+    </div>
+    <div className="text-gray-300 transition-colors duration-300">
+    Configure filters to quickly sort out the  right publication made from the list.
+    </div>
+    <div className="text-gray-300 transition-colors duration-300">
+    View, edit, and trash them whenever needed. Facilitate effective connections
+    </div>
+    <div className="text-gray-300 transition-colors duration-300"> 
+    between research scholars and their guides.
+    </div>
+    <div className="text-gray-300 transition-colors duration-300">
+      <strong>Do more with Creatrix:</strong>
+    </div>
+    <ul className="list-disc list-inside text-gray-300 transition-colors duration-300">
+      <li>Add/Update thesis online</li>
+      <li>Check comments of Supervisors and Chairmain</li>
+      <li>Status notification for submissions</li>
+    </ul>
+  </div>
+
+  {/* Image Section 2 */}
+  <div className="image-section">
+    <img 
+      src="Keep-publications-on-track.webp" 
+      alt="Description" 
+      style={{ width: '150%', height: '150%' }}
+    />
+  </div>
+</div>
+
+
+
+        </>
+    );
+}
+
+export default Body3sec;
