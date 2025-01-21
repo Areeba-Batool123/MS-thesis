0708cf90e7f72cde9ea67ec59e252c14e10d6cdb
Dark world <131669303+AsifaSiraj@users.noreply.github.com>
2025-01-21 19:19:39 +0500

    ServiceFolder

---
+++ src/assets/Home/Check.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Check() {
+  return (
+    <>
+    import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Services from './Services'; // Adjust the path if necessary
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/services" element={<Services />} />
+        {/* Add other routes as needed */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+    </>
+  );
+};
+
+export default Check;
